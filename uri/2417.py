0e80accb3fa32b8fd5eeec83bfc52ad6f12e6be1
emanuelle guse <120527386+manuguse@users.noreply.github.com>
2023-03-16 12:31:57 -0300

    Update 2417.py

--- uri/2417.py
+++ uri/2417.py
@@ -1,23 +1,23 @@
 # exercício 2417
 
-    vitoriaC = int(input('vitórias cormengo '))
-    empateC = int(input('empates cormengo ')) 
-    saldoC = int(input('saldo cormengo '))
+vitoriaC = int(input('vitórias cormengo '))
+empateC = int(input('empates cormengo ')) 
+saldoC = int(input('saldo cormengo '))
 
-    vitoriaF = int(input('vitórias flaminthians '))
-    empateF = int(input('empates flaminthians ')) 
-    saldoF = int(input('saldo flaminthians '))
+vitoriaF = int(input('vitórias flaminthians '))
+empateF = int(input('empates flaminthians ')) 
+saldoF = int(input('saldo flaminthians '))
 
-    pontosC = vitoriaC*3 + empateC
-    pontosF = vitoriaF*3 + empateF
+pontosC = vitoriaC*3 + empateC
+pontosF = vitoriaF*3 + empateF
 
-    if (pontosC > pontosF):
-        print('C')
-    elif (pontosF > pontosC):
-        print('F')
-    elif (saldoC > saldoF):
-        print('C')
-    elif (saldoF > saldoC):
-        print('F')
-    else:
-        print('=')
+if (pontosC > pontosF):
+    print('C')
+elif (pontosF > pontosC):
+    print('F')
+elif (saldoC > saldoF):
+    print('C')
+elif (saldoF > saldoC):
+    print('F')
+else:
+    print('=')
