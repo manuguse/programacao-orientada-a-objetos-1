79cfc1215797618ebcdf245a27a014e19f5dbb72
emanuelle guse <120527386+manuguse@users.noreply.github.com>
2023-03-17 11:40:37 -0300

    Create 1006.py

---
+++ uri/1006.py
@@ -0,0 +1,7 @@
+# exemplo 1006
+
+valor1 = float(input('valor 1 '))
+valor2 = float(input('valor 2 '))
+valor3 = float(input('valor 3 '))
+media = float((valor1 + valor2 + valor3)/3)
+print('MEDIA = {:.2f}'.format(media)) 
