637377ad42893a8c97924b3594c3e40ddd24d9de
emanuelle guse <120527386+manuguse@users.noreply.github.com>
2023-03-17 11:48:38 -0300

    Create 1008.py

---
+++ uri/1008.py
@@ -0,0 +1,9 @@
+# exemplo 1008
+
+numero = int(input('numero '))
+salarioHora = int(input('salário por hora '))
+hora = float(input('horas trabalhadas '))
+salario = float(hora*salarioHora)
+
+print('NUMBER =', numero)
+print('SALARY = U$ {:.2f}'.format(salario))
