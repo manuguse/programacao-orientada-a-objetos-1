5011fcec943c5c94e01cff8f22049b838703b9f4
emanuelle guse <120527386+manuguse@users.noreply.github.com>
2023-03-17 11:41:04 -0300

    Create 1007.py

---
+++ uri/1007.py
@@ -0,0 +1,8 @@
+# exemplo 1007
+
+valor1 = int(input('valor 1 '))
+valor2 = int(input('valor 2 '))
+valor3 = int(input('valor 3 '))
+valor4 = int(input('valor 4 '))
+diferenca = int(valor1*valor2 - valor3*valor4)
+print('DIFERENCA =', diferenca) 
