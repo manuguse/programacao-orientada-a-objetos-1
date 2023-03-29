73735cf20becf30218de195940fdb7639820ef57
emanuelle guse <120527386+manuguse@users.noreply.github.com>
2023-03-29 15:36:51 -0300

    Update 2373.py

--- uri/2373.py
+++ uri/2373.py
@@ -1,9 +1,19 @@
 cont = 0
-n = int(input('bandejas '))
+while True:
+    n = int(input('bandejas '))
+    if (1 <= n <= 100):
+        break
 
 for i in range(n):
-    lata = int(input('lata '))
-    copo = int(input('copo '))
+    while True:
+        lata = int(input('lata '))
+        if (1 <= lata <= 100):
+            break
+    while True:
+        copo = int(input('copo '))
+        if (1 <= copo <= 100):
+            break
+
     if lata > copo:
         cont += copo
 print(cont)
